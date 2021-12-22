7aba1a970850ba9fb30d9d833a277fc76370dac4
EuRV <jugin_omsk@mail.ru>
2021-12-22 22:58:28 +0600

    Fix index.js
    Introduced repetitive logic from other modules. Renamed variables.
    Fixed import/export.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,26 @@
 import readlineSync from 'readline-sync';
-import sayUserName from './cli.js';
 
-const basement = (rounds, rulesGame, checkGame) => {
-  const userName = sayUserName();
+const rounds = 3;
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min) + min);
+
+function basement(rulesGame, checkGame) {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
   console.log(`${rulesGame}`);
 
   for (let i = 0; i < rounds; i += 1) {
-    const [randomNumbers, hasEvenNumber] = checkGame[i];
-    console.log(`Question: ${randomNumbers}`);
+    const [questionValue, answerValue] = checkGame[i];
+    console.log(`Question: ${questionValue}`);
     const askAnswer = readlineSync.question('Your answer: ');
-    if (askAnswer !== hasEvenNumber) {
-      console.log(`'${askAnswer}' is wrong answer ;(. Correct answer was '${hasEvenNumber}'.`);
+    if (askAnswer !== answerValue.toString()) {
+      console.log(`'${askAnswer}' is wrong answer ;(. Correct answer was '${answerValue}'.`);
       console.log(`Let's try again, ${userName}!`);
       return;
     }
     console.log('Correct!');
   }
   console.log(`Congratulations, ${userName}!`);
-};
+}
 
-export default basement;
+export { rounds, getRandomNumber, basement };
